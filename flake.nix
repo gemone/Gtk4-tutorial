8245090a596d8b55a8925d5b49c1fee323d5f650
Gemone <git@gemo.one>
2024-01-15 14:54:46 +0800

    chore: add flake env (devenv)

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+      });
+
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  # https://devenv.sh/reference/options/
+                  packages = with pkgs.buildPackages; [
+                    patchelf
+                    pkg-config
+                    dbus
+                    glib
+                    gdk-pixbuf
+                    gtk4
+                    pango
+                    cairo
+                    graphene
+                    harfbuzz
+                  ];
+                  languages.rust.enable = true;
+                  languages.c.enable = true;
+                }
+              ];
+            };
+          });
+    };
+}
